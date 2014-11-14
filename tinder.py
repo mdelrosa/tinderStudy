2b38581b99a8b1e4e7d6c67428be198d78892430
mdelrosa <mason.delro@gmail.com>
2014-11-13 19:39:07 -0500

    Estimating PDF based on data

--- tinder.py
+++ tinder.py
@@ -5,3 +5,22 @@ from __future__ import print_function, division
 import numpy
 import thinkbayes2
 import thinkplot
+
+
+def main():
+	maleRates = [0.52, 0.38, 0.39, 1.01, 2.63]
+	femaleRates = [50, 20.5]
+	pdfMale = thinkbayes2.EstimatedPdf(maleRates)
+	pdfFemale = thinkbayes2.EstimatedPdf(femaleRates)
+	low, high = 0, 100
+	n = 101
+	xs = numpy.linspace(low, high, n)
+	pmfMale = pdfMale.MakePmf(steps=xs)
+	pmfFemale = pdfFemale.MakePmf(steps=xs)
+
+	thinkplot.Pdf(pdfMale, label='Male Prior')
+	thinkplot.Pdf(pdfFemale, label='Female Prior')
+	thinkplot.show()
+
+if __name__ == '__main__':
+	main()
